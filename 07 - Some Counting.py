78ca77957cff348973244138b69b7c49a66926d2
brionesmaxine11-droid <brionesmaxine11@gmail.com>
2025-10-22 21:45:13 +0400

    Add files via upload

---
+++ 07 - Some Counting.py
@@ -0,0 +1,22 @@
+# Exercise 7: Some Counting - 20 Marks
+# Maxine Briones - CS4
+
+print("Counting up from 0 to 50:")                # Write a loop that counts up from 0 to 50 in increments of 1.
+for x in range(0, 51):
+     print(x)
+
+print("\nCounting down from 50 to 0:")            # Write a loop that counts down from 50 to 0 in decrements of 1.
+for y in range(50, -1, -1):
+     print(y)
+
+print("\nCounting up from 30 to 50:")             # Write a loop that counts up from 30 to 50 in increments of 1.
+for z in range(30, 51):
+     print(z)
+
+print("\nCounting down from 50 to 10 using 2")    # Write a loop that counts up from 30 to 50 in increments of 1.
+for a in range (50, 8, -2):
+     print(a)
+
+print("\n Counting up from 100 to 200 using 5")   # Write a loop that counts up from 100 to 200 in increments of 5. 
+for b in range (100, 205, 5):
+      print(b)
