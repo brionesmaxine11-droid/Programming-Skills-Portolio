69d6ef8c529017a25fef8d139dd37fd9a279999c
brionesmaxine11-droid <brionesmaxine11@gmail.com>
2025-10-22 21:41:29 +0400

    Add files via upload

---
+++ 02 - Simple Sums.py
@@ -0,0 +1,7 @@
+#Exercise 2: Simple Sums - 15 Marks
+# Maxine Briones - CS4
+a = 8               # Declare a variable and initialize it with the integer value 8.
+b = 10              # Declare a second variable and initialize it with the integer value 10.
+sum = int(a+b)      # Declare a third variable that stores the sum of first two numbers.
+
+print(sum)          # Print the value of the sum to the console
