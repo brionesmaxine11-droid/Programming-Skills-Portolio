cb286702ac9dd4f22241943a716ab334fc8b9998
brionesmaxine11-droid <brionesmaxine11@gmail.com>
2025-10-22 21:46:07 +0400

    Add files via upload

---
+++ 09 - Hello.py
@@ -0,0 +1,11 @@
+# Exercise 9: Hello - 10 Marks
+# Maxine Briones - CS4
+
+def hello():
+    print ("Hello") # Fill in this blank to print "Hello" to the console
+
+def main():
+    hello()         # Fill in this blank to print "Hello" to the console
+
+if __name__ == "__main__":
+ main()
