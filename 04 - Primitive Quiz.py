34195c93318d1cdd7f4ba9dbf8e15ca631f9c4aa
brionesmaxine11-droid <brionesmaxine11@gmail.com>
2025-10-22 21:44:06 +0400

    Add files via upload

---
+++ 04 - Primitive Quiz.py
@@ -0,0 +1,73 @@
+# Exercise 4: Primitive Quiz - 30 Marks
+# Maxine Briones - CS4
+
+answer = input("What is the capital of France? ")     # Write a program that asks the user "What is the capital of France?" and waits for their response.
+
+if answer == "Paris":   
+    print("Correct! \n")                              # If the user's answer is correct (i.e., "Paris"), the program should print a message saying the answer is correct.
+else:
+    print("Wrong, the correct answer is Paris. \n")   # If the answer is incorrect, the program should print a message saying the answer is wrong.
+
+
+#Advanced Requirements 
+print("! Advanced Requirements ! \n")
+
+answer = input("1. What is the capital of France? ")
+if answer.lower() == "paris":   
+    print("Correct! \n") 
+else:
+    print("Wrong, the correct answer is Paris.\n")
+
+answer = input("2. What is the capital of Sweden? ")
+if answer.lower() == "stockholm":   
+    print("Correct! \n")
+else:
+    print("Wrong, the correct answer is Stockholm. \n")
+
+answer = input("3. What is the capital of the United Kingdom? ")
+if answer.lower() == "london":   
+    print("Correct! \n")
+else:
+    print("Wrong, the correct answer is London.\n ")
+
+answer = input("4. What is the capital of Germany? ")
+if answer.lower() == "berlin":   
+    print("Correct! \n")
+else:
+    print("Wrong, the correct answer is Berlin. \n")
+
+answer = input("5. What is the capital of Greece? ")
+if answer.lower() == "athens":   
+    print("Correct! \n")
+else:
+    print("Wrong, the correct answer is Athens. \n")
+
+answer = input("6. What is the capital of Russia? ")
+if answer.lower() == "moscow":   
+    print("Correct! \n")
+else:
+    print("Wrong, the correct answer is Moscow. \n")
+
+answer = input("7. What is the capital of Italy? ")
+if answer.lower() == "rome":   
+    print("Correct! \n")
+else:
+    print("Wrong, the correct answer is Rome. \n")
+
+answer = input("8. What is the capital of the Netherlands? ")
+if answer.lower() == "amsterdam":   
+    print("Correct! \n")
+else:
+    print("Wrong, the correct answer is Amsterdam. \n")
+
+answer = input("9. What is the capital of Spain? ")
+if answer.lower() == "madrid":   
+    print("Correct! \n")
+else:
+    print("Wrong, the correct answer is Madrid. \n")
+
+answer = input("10. What is the capital of Ireland? ")
+if answer.lower() == "dublin":   
+    print("Correct! \n")
+else:
+    print("Wrong, the correct answer is Dublin. \n")
