ca02936cb33d6ab65347088e65076000ce4e2d91
brionesmaxine11-droid <brionesmaxine11@gmail.com>
2025-10-22 21:40:49 +0400

    Add files via upload

---
+++ 01 - CodingIsCool.py
@@ -0,0 +1,9 @@
+# Exercise 1: Coding is Cool- 10 Marks
+# Maxine Briones - CS4
+
+word1 = ("Coding ")
+word2 = ("is ")
+word3 = ("Cool")
+
+# Use string concatenation to combine the variables and print the phrase
+print(word1 + word2 + word3)
